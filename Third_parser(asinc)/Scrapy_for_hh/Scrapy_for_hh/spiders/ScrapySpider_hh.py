51aacd7ee442c7a543b0c11b2f3e79f760e3be9b
Uncle-Fox <Nemogemut@gmail.com>
2024-01-04 02:46:55 +0300

    добавили scrapy

---
+++ Third_parser(asinc)/Scrapy_for_hh/Scrapy_for_hh/spiders/ScrapySpider_hh.py
@@ -0,0 +1,31 @@
+from typing import Iterable
+import scrapy
+from urllib.parse import urlparse
+import time
+from scrapy.http import Request
+
+
+class ScrapyspiderHhSpider(scrapy.Spider):
+    name = "ScrapySpider_hh"
+    allowed_domains = ["hh.ru"]
+    start_urls = ["https://hh.ru/search/vacancy?text=программист+python&area=113&customDomain=1&page="]
+
+    def start_requests(self) -> Iterable[Request]:
+        base_url = self.start_urls[0]
+        for page_number in range (1, 40):
+            url = f"{base_url}{page_number}"
+            yield scrapy.Request(url=url, callback=self.parse)
+            time.sleep(1)
+
+    def parse(self, response):
+        vacancys = response.xpath('//div[contains(@class, "vacancy-serp-item")]')
+        processed_urls = set()
+        for vacancy in vacancys:
+            url = vacancy.xpath('.//span/a[contains(@data-qa, "serp-item__title") and contains(@class, "serp-item__title")]/@href').extract_first()
+            if url and url not in processed_urls:
+                processed_urls.add(url)
+                yield {
+                    'name': vacancy.xpath('.//span/a[contains(@data-qa, "serp-item__title") and contains(@class, "serp-item__title")]/text()').get(),
+                    'salary': vacancy.xpath('.//span[contains(@data-qa, "vacancy-serp__vacancy-compensation")]/text()').get(),
+                    'url': url.split('?')[0]
+                }
